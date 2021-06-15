71f285552f304125cdda1dbc359ecac5b5669b21
R-thinking <Making.R.W@gmail.com>
2021-06-15 10:50:03 +0900

    CSS: Global set-up

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 'use strict';
+const body = document.querySelector('body');
+let timer = 0;
+window.addEventListener('scroll', () => handleScroll(), true);
+
+function handleScroll() {
+    if (body.classList.contains("on-scrollbar") === false) {
+        clearTimeout(timer);
+        body.classList.add("on-scrollbar");
+    }
+    else if (body.classList.contains("on-scrollbar") === true) {
+        clearTimeout(timer);
+        timer = setTimeout(()=>body.classList.remove('on-scrollbar'),1800);
+    }
+}
+
+
+
+
+
